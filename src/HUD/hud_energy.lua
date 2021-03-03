d96d291aa7f3e3414ad997a1c4e2f6a021e18e0e
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2021-03-03 13:57:56 +0100

    Better energy bar

--- src/HUD/hud_energy.lua
+++ src/HUD/hud_energy.lua
@@ -1,18 +1,16 @@
 function block_league.energy_create(arena, p_name)
 
-  local p_energy = arena.players[p_name].energy
-
   Panel:new("bl_energy", {
     player = p_name,
-    bg = "",
+    bg = "bl_hud_energy_empty.png",
     title = "",
     position = { x = 0.5, y = 1 },
-    alignment = { x = -1, y = 0 },
+    alignment = { x = 1, y = 0 },
+    offset = { x = 20, y = -76},
+    bg_scale = { x = 1.5, y = 1.5},
     sub_img_elems = {
       energy_indicator = {
-        scale = {x = p_energy/6.5, y = 1.3},
-        offset = {x = 20, y = -78},
-        alignment = { x = 1, y = 0 },
+        offset = { x = 5 },
         text = "bl_hud_energy.png"
       },
     }
@@ -24,10 +22,11 @@ end
 function block_league.energy_update(arena, p_name)
 
   local panel = panel_lib.get_panel(p_name, "bl_energy")
+  local energy = arena.players[p_name].energy
 
   panel:update(nil,
     {energy_indicator = {
-      scale = {x = arena.players[p_name].energy/6.5, y = 1.2},
+      scale = {x = (energy / 100) * 1.5, y = 1.5},
     }}
   )
 
