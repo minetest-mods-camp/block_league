f0c035243c929be9b27b5eea088e90fc8aa9787f
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2022-12-26 12:19:09 +0100

    grant admins the privilege to open players' profiles (or I don't know how to let the former open their profile through command blocks)

--- src/commands.lua
+++ src/commands.lua
@@ -99,6 +99,14 @@ ChatCmdBuilder.new("bladmin", function(cmd)
       block_league.enter_test_mode(sender)
     end)
 
+    -- need it to allow command blocks to run /bladmin @nearest, or I don't know how to open players' profiles through pressure plates and the like
+    cmd:sub("profile :playername", function(sender, p_name)
+      if arena_lib.is_player_in_arena(sender) then
+        minetest.chat_send_player(sender, minetest.colorize("#e6482e", S("[!] You can't perform this action right now!")))
+        return end
+
+      block_league.show_profile(p_name)
+    end)
 
 end, {
   description = S("mod management"),
